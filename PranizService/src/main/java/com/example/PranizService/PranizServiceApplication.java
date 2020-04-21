87125af89762591e954e1f60a0993c16afc44eba
Pranisa <pranisa.g@telogic.com.sg>
2020-04-21 11:22:35 +0700

    first commit

---
+++ PranizService/src/main/java/com/example/PranizService/PranizServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.PranizService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PranizServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PranizServiceApplication.class, args);
+	}
+
+}
